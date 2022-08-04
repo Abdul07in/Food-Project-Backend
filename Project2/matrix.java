dd9e31606f882a89c0ad0e663f371844dfd0f63e
Abdul Majeed <abdulkanoor@gmail.com>
2022-08-04 17:14:01 +0000

    Test commit

---
+++ Project2/matrix.java
@@ -0,0 +1,16 @@
+package Project2;
+
+import java.util.Scanner;
+
+public class matrix{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println(num);
+        sc.close();
+    }
+
+}
