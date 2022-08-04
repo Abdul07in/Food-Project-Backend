0c14f4a1d38984daa18d1e1d7e784c4578a84813
Abdul Majeed <abdulkanoor@gmail.com>
2022-08-04 18:40:12 +0000

    new edit

--- Project2/matrix.java
+++ Project2/matrix.java
@@ -3,13 +3,19 @@ package Project2;
 import java.util.Scanner;
 
 public class matrix{
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a number: ");
+        System.out.print("Enter a num1: ");
         int num = sc.nextInt();
 
-        System.out.println(num);
+        System.out.print("Enter a num2:");
+        int num2 = sc.nextInt();
+
+        System.out.println("Addition is " + (num+num2));
         sc.close();
     }
 
