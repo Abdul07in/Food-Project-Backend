84653d590f018218c0e1567d0e7e6e3c57060120
Abdul Majeed <63016696+Abdul07in@users.noreply.github.com>
2023-01-10 16:26:57 +0000

    Update 1.2.1

--- Core Food Project/src/com/food/dao/FoodDao.java
+++ Core Food Project/src/com/food/dao/FoodDao.java
@@ -1,6 +1,18 @@
 package com.food.dao;
 
+import java.util.List;
+
+import com.food.pojo.Food;
+
 public interface FoodDao {
     
+    public List<Food> showAll();
+    public boolean updateItem(Food food);
+    public boolean insertItem(Food food);
+    public boolean deleteItem(Food food);
+
+    public Food searchById(Integer foodId);
+    public Food searchByName(String foodName);
+    public List<Food> searchByType(Integer foodId);
     
 }
